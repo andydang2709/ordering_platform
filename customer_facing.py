8bd94609f785fcb9ad86adaf7d03518de19361d5
Andy Dang <andykhangdang@gmail.com>
2025-02-14 10:20:44 -0500

    create django project

--- customer_facing.py
+++ customer_facing.py
@@ -1,5 +1,17 @@
 # TODO: Import necessary packages
+# pip install django streamlit numpy pandas matplotlib seaborn scikit-learn sqlalchemy python-dotenv cryptography
+# source .venv/bin/activate
+# streamlit run customer_facing.py
 
-# TODO: Define the relevant classes
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
 
-# TODO: Turn Layout Web Design into Code
+# TODO: Initialize the app
+st.title("Homestead Coffee Order")
+st.sidebar.header("Options")
